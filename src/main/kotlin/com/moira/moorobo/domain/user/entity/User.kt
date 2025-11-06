fba5f2992bc2b24ac792adbcc6daa52d540f4028
hunzzzzz <hun819.st@gmail.com>
2025-11-06 20:10:43 +0900

    User 엔티티 생성

---
+++ src/main/kotlin/com/moira/moorobo/domain/user/entity/User.kt
@@ -0,0 +1,27 @@
+package com.moira.moorobo.domain.user.entity
+
+import com.moira.moorobo.global.entity.BaseEntity
+import jakarta.persistence.*
+import java.util.UUID
+
+@Entity
+@Table(name = "USER", schema = "MOO_ROBO")
+class User(
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    var id: String? = UUID.randomUUID().toString(),
+
+    @Column(name = "name", nullable = false)
+    var name: String,
+
+    @Column(name = "email", nullable = false, unique = true)
+    var email: String,
+
+    @Column(name = "password", nullable = false)
+    var password: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    var role: UserRole
+) : BaseEntity() {
+}
---
+++ src/main/kotlin/com/moira/moorobo/domain/user/entity/UserRole.kt
@@ -0,0 +1,5 @@
+package com.moira.moorobo.domain.user.entity
+
+enum class UserRole {
+    STUDENT, TEACHER, ADMIN
+}
